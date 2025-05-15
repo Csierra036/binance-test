03afe6dd042323887dd71f6ad41821bb2f0076b5
Cristian Sierra <cristiansierra.rap@gmail.com>
2025-05-15 19:53:27 -0400

    feat: Create order with binance pay

--- src/binance/binance.controller.ts
+++ src/binance/binance.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { BinanceService } from './binance.service';
 import { Get } from '@nestjs/common';
 import { WalletRestAPI } from '@binance/wallet';
@@ -7,13 +7,20 @@ import { WalletRestAPI } from '@binance/wallet';
 export class BinanceController {
     constructor(private readonly binance_service: BinanceService) {}
 
-    @Get('get-account-info')
-    getAccountInfo(): Promise<WalletRestAPI.AccountInfoResponse> {
-        return this.binance_service.GetAccountInfo();
-    }
-    
-    @Get('check-wallet')
-    getWallet(): Promise<WalletRestAPI.QueryUserWalletBalanceResponse >{
-        return this.binance_service.GetCheck();
+    @Post()
+    async createOrder(){
+        const payload = {
+            merchantTradeNo: `order-${Date.now()}`,
+            orderAmount: 0.05,
+            currency: 'USDT',
+            goods: {
+                goodsType: '01',
+                goodsCategory: 'D000',
+                referenceGoodsId: 'example-product',
+                goodsName: 'Ejemplo',
+                goodsDetail: 'Descripción del producto',
+            },
+        };
+        return await this.binance_service.createOrder(payload);  
     }
 }
--- src/binance/binance.service.ts
+++ src/binance/binance.service.ts
@@ -1,51 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { Wallet, WalletRestAPI } from '@binance/wallet';
 import { ConfigService } from '@nestjs/config';
+import axios from 'axios';
+import * as crypto from 'crypto'
+
 
 @Injectable()
 export class BinanceService {
-    private client: Wallet
-
-    //Constructor
+    private apiKey;
+    private apiSecret;
+    private endpoint;
     constructor(private readonly configService: ConfigService){
-        const apiKey = this.configService.get<string>('BINANCE_API_KEY');
-        const apiSecret = this.configService.get<string>('BINANCE_API_SECRET');
-
-        if (!apiKey || !apiSecret) {
-            throw new Error('Missing Binance API key or secret in configuration');
-        }
-
-        const configurationRestAPI = {
-            apiKey,
-            apiSecret,
-        };
-
-        this.client = new Wallet({ configurationRestAPI });
+      this.apiKey = this.configService.get<string>('BINANCE_API_KEY');
+      this.apiSecret = this.configService.get<string>('BINANCE_API_SECRET');
+      this.endpoint = 'https://bpay.binanceapi.com/binancepay/openapi/order';
     }
-    //functions
+    
+    
+    async createOrder(orderPayload: any): Promise<any> {
+      const timestamp = Date.now();
+      const nonce = crypto.randomBytes(16).toString('hex');
+      const payload = JSON.stringify(orderPayload);
 
-    GetAccountInfo(): Promise<WalletRestAPI.AccountInfoResponse> {
-        return this.client.restAPI
-          .accountInfo()
-          .then((res) => res.data())
-          .then((data: WalletRestAPI.AccountInfoResponse) => {
-            return data;
-          })
-          .catch((err) => {
-            console.error(err);
-            throw new Error('Failed to fetch Binance account info');
-          });
-      }
+      const signature = crypto
+        .createHmac('sha512', this.apiSecret)
+        .update(payload + `\n${timestamp}\n${nonce}\n`)
+        .digest('hex');
 
-    GetCheck(): Promise<WalletRestAPI.QueryUserWalletBalanceResponse> {
-    return this.client.restAPI.queryUserWalletBalance()
-        .then((res) => res.data())
-        .then((data: WalletRestAPI.QueryUserWalletBalanceResponse) => {
-        return data;
-        })
-        .catch((err) => {
-        console.error(err);
-        throw new Error('Failed to fetch Binance account info');
-        });
+      const headers = {
+        'Content-Type': 'application/json',
+        'BinancePay-Timestamp': timestamp,
+        'BinancePay-Nonce': nonce,
+        'BinancePay-Certificate-SN': this.apiKey,
+        'BinancePay-Signature': signature,
+      };
+
+      const response = await axios.post(this.endpoint, orderPayload, { headers });
+      return response.data;
     }
 }
