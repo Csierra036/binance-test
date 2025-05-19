a5b434a72b5209c99f02117657f4abefdf21ef3f
Cristian Sierra <cristiansierra.rap@gmail.com>
2025-05-19 15:11:55 -0400

    fix: Create a order in binance
    Fixed the error that existed when creating a payment order in binance.

--- src/binance/binance.controller.ts
+++ src/binance/binance.controller.ts
@@ -1,26 +1,13 @@
 import { Controller, Post } from '@nestjs/common';
 import { BinanceService } from './binance.service';
-import { Get } from '@nestjs/common';
-import { WalletRestAPI } from '@binance/wallet';
+
 
 @Controller('binance')
 export class BinanceController {
     constructor(private readonly binance_service: BinanceService) {}
 
     @Post('create-order')
-    async createOrder(){
-        const payload = {
-            merchantTradeNo: 'order-001',
-            orderAmount: 0.50,
-            currency: 'USDT',
-            goods: {
-                goodsType: '01',
-                goodsCategory: 'D000',
-                referenceGoodsId: 'example-product',
-                goodsName: 'Example',
-                goodsDetail: 'Descripción del producto',
-            },
-        };
-        return await this.binance_service.createOrder(payload);  
+    async createOrder() {
+        return this.binance_service.createBinancePayOrder();
     }
 }
--- src/binance/binance.service.ts
+++ src/binance/binance.service.ts
@@ -1,48 +1,109 @@
 import { Injectable } from '@nestjs/common';
-import { Wallet, WalletRestAPI } from '@binance/wallet';
 import { ConfigService } from '@nestjs/config';
 import axios from 'axios';
-import * as crypto from 'crypto'
-
+import * as crypto from 'crypto';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class BinanceService {
-  private apiKey;
-  private apiSecret;
-  private endpoint;
-  constructor(private readonly configService: ConfigService){
-    this.apiKey = this.configService.get<string>('BINANCE_API_KEY');
-    this.apiSecret = this.configService.get<string>('BINANCE_API_SECRET');
+  private apiKey: string;
+  private apiSecret: string;
+  private endpoint: string;
+
+  constructor(private readonly configService: ConfigService) {
+    this.apiKey = this.checkEnvConfig('BINANCE_API_KEY');
+    this.apiSecret = this.checkEnvConfig('BINANCE_API_SECRET');
     this.endpoint = 'https://bpay.binanceapi.com/binancepay/openapi/v3/order';
   }
-    
-    
-  async createOrder(orderPayload: any): Promise<any> {
+
+
+  private checkEnvConfig(key: string): string {
+    const value = this.configService.get<string>(key);
+    if (!value) {
+      throw new Error(`This variable is requeried: ${key}`);
+    }
+    return value;
+  }
+
+
+  private signPayload(payload: any, timestamp: any, nonce: string): string {
+    const dataToSign = `${timestamp}\n${nonce}\n${payload}\n`;
+    return crypto.createHmac("sha512", this.apiSecret).update(dataToSign).digest("hex").toUpperCase();
+  }
+
+
+  private generateMerchantTradeNo(): string {
     const timestamp = Date.now().toString();
-    const nonce = crypto.randomBytes(16).toString('hex');
-    const payloadStr = JSON.stringify(orderPayload);
+    const randomSuffix = Math.floor(Math.random() * 9000 + 1000);
+    return `${timestamp}${randomSuffix}`;
+  }
 
-    const message = 'timestamp=1578963600000';
-    
-    const signature = crypto
-      .createHmac('sha512', this.apiSecret)
-      .update(message)
-      .digest('hex');
 
-    const headers = {
-      'Content-Type': 'application/json',
-      'BinancePay-Timestamp': timestamp,
-      'BinancePay-Nonce': nonce,
-      'BinancePay-Certificate-SN': this.apiKey,
-      'BinancePay-Signature': signature,
+  async createBinancePayOrder() {
+    const timestamp = Date.now().toString(); 
+    const nonce = uuidv4().replace(/-/g, ''); //ID not repeteable
+    const merchantTradeNo = this.generateMerchantTradeNo();
+    const payload = {
+      env: {
+        terminalType: "WEB",
+      },
+      merchantTradeNo: merchantTradeNo,
+      orderAmount: 0.10,
+      currency: "USDT",
+      description: "very good Ice Cream",
+      goodsDetails: [
+        {
+          goodsType: "01",
+          goodsCategory: "D000",
+          referenceGoodsId: "7876763A3B",
+          goodsName: "Ice Cream",
+          goodsDetail: "Greentea ice cream cone",
+        },
+      ],
     };
 
+    const payloadStr = JSON.stringify(payload);
+    const signature = this.signPayload(payloadStr, timestamp, nonce);
+
     try {
-      const response = await axios.post(this.endpoint, orderPayload, { headers, maxBodyLength: Infinity });
-      return response.data;
+      const response = await axios.post(
+        this.endpoint,
+        payload,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "BinancePay-Timestamp": timestamp.toString(),
+            "BinancePay-Nonce": nonce,
+            "BinancePay-Certificate-SN": this.apiKey,
+            "BinancePay-Signature": signature,
+          },
+        }
+      );
+
+      const data = response.data;
+      console.log("Response data:", data);
+      console.log("Response status:", response.status);
+      if (data.status === "SUCCESS") {
+        return {
+          statusCode: 201,
+          message: "Order create successfully.",
+          checkoutUrl: data.data.checkoutUrl,
+          orderId: data.data.orderId,
+          tradeNo: nonce
+        };
+      } else {
+        return {
+          statusCode: 400,
+          message: "Error in create the order",
+          error: data
+        };
+      }
     } catch (error) {
-      console.error('❌ Error al crear la orden:', error.response?.data || error.message);
-      throw error;
+      return {
+        statusCode: 500,
+        message: "Error in the request: ",
+        error: error.response?.data || error.message
+      };
     }
   }
 }
