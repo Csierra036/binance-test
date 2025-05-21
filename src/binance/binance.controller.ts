c426caa12640a4829795e103efef40279171f18d
Cristian Alexander Sierra <159510185+Csierra036@users.noreply.github.com>
2025-05-21 18:57:54 -0400

    Merge pull request #2 from Csierra036/feature/Close-order-service
    feat: Close Order service

--- src/binance/binance.controller.ts
+++ src/binance/binance.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post,Delete } from '@nestjs/common';
 import { BinanceService } from './binance.service';
 import { CreateOrder } from './dto/create.order.dto';
-import { Body } from '@nestjs/common';
+import { CloseOrder } from './dto/close.order.dto';
+import { Body, Query } from '@nestjs/common';
 @Controller('binance')
 export class BinanceController {
     constructor(private readonly binance_service: BinanceService) {}
@@ -9,5 +10,11 @@ export class BinanceController {
     @Post('create-order')
     async createOrder(@Body() CreateOrder: CreateOrder) {
         return this.binance_service.createBinancePayOrder(CreateOrder);
+    }
+    @Delete('delete-order')
+    async closeOrder(@Query('id') id: string){
+        const request = new CloseOrder();
+        request.merchantTradeNo = id;
+        return this.binance_service.CloseBinancePayOrder(request)
     }
 }
